c693f1fd3210179044e7760f5bb1f8cafb779bd2
Dunia Kreasi <japriproject@gmail.com>
2026-06-27 20:49:42 +0800

    uploads

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracingRoot: process.cwd(),
+  turbopack: {
+    root: process.cwd(),
+  },
   allowedDevOrigins: ['72.62.121.108'],
   experimental: {
     serverActions: {
